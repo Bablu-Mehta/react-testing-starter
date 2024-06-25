3d74359f7014a3a94839619f00a823bf65fc3a5a
Bablu Mehta <babloomehta895012@gmail.com>
2024-06-25 08:34:46 +0530

    texting expandanble text

--- src/pages/PlaygroundPage.tsx
+++ src/pages/PlaygroundPage.tsx
@@ -1,8 +1,10 @@
+import ExpandableText from "../components/ExpandableText";
 import Onboarding from "../components/Onboarding";
 import TermsAndConditions from "../components/TermsAndConditions";
 
 const PlaygroundPage = () => {
-  return <TermsAndConditions />;
+  
+  return <ExpandableText text="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Perferendis nulla nihil debitis in. Laudantium minima ipsa minus, aperiam est perspiciatis, voluptatibus voluptas accusantium dolorum earum porro dolore. Aperiam, tempore distinctio itaque pariatur, nobis natus perspiciatis ex eum deserunt laborum cum rerum dicta optio. Veritatis explicabo, doloribus asperiores iste nostrum repudiandae suscipit alias, officiis assumenda atque inventore ea adipisci, autem itaque aliquam minima aspernatur ut accusamus eveniet? Dignissimos totam voluptates cum eum quaerat ratione magnam numquam, iusto repellendus reiciendis, enim voluptatum saepe ipsam eveniet fugiat beatae repudiandae aut expedita fugit? Sunt ducimus ipsam autem necessitatibus tempore saepe voluptatem corrupti voluptate ipsa.s" />;
 };
 
 export default PlaygroundPage;
